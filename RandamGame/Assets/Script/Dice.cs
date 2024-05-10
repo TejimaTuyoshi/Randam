ea8785d7631121c6a48ca68e89b9b8235c4cadb4
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-05-10 10:43:20 +0900

    ダイス時の判定タイミングの変更

--- RandamGame/Assets/Script/Dice.cs
+++ RandamGame/Assets/Script/Dice.cs
@@ -21,15 +21,15 @@ public class Dice : MonoBehaviour
     [SerializeField] int _int = 9;
     [SerializeField] int _edu = 9;
     [SerializeField] int _hp = 45;
-    bool _resultST = false;
-    bool _resultC = false;
-    bool _resultP = false;
-    bool _resultD = false;
-    bool _resultA = false;
-    bool _resultSI = false;
-    bool _resultI = false;
-    bool _resultE = false;
-    bool _dice = false;
+    [SerializeField] bool _resultST = false;
+    [SerializeField] bool _resultC = false;
+    [SerializeField] bool _resultP = false;
+    [SerializeField] bool _resultD = false;
+    [SerializeField] bool _resultA = false;
+    [SerializeField] bool _resultSI = false;
+    [SerializeField] bool _resultI = false;
+    [SerializeField] bool _resultE = false;
+    [SerializeField] bool _result = false;
     [SerializeField] Text text;
     [SerializeField] Text text2;
     [SerializeField] Text text3;
@@ -45,70 +45,19 @@ public class Dice : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
         text2.text = ("STR:" + _str + " CON:" + _con + " POW:" + _pow + " DEX:" + _dex + " APP:" + _app + " SIZ:" + _siz + " INT:" + _int + " EDU:" + _edu);
-        text3.text = ("HP:" + _hp + " / 45");
-        var random = new System.Random();         
-        var number = random.Next(00, 100);   // 100面ダイス用
-        var num = random.Next(01, 07);//6面ダイス用
-        var num2 = random.Next(01, 04);//3面ダイス用
-        var num3 = random.Next(01, 05);//4面ダイス用
-        var num4 = random.Next(01, 04) + random.Next(01, 04);//3面2回ダイス用
-        var num5 = random.Next(01, 21);//20面ダイス用
-        _move = num;
-        _hund = number.ToString().PadLeft(2, '0');
-        _six = num.ToString().PadLeft(1, '0');
-        _three = num2.ToString().PadLeft(1, '0');
-        _four = num3.ToString().PadLeft(1, '0');
-        _threeToTwo = num4.ToString().PadLeft(1, '0');
-        _twenty = num5.ToString().PadLeft(2, '0');
-        if (_str * 5 >= number) 
+        text3.text = ("HP:" + _hp + " / 45"); 
+
+        if (!_resultST && _result == true)
         {
-            _resultST = true;
-        }
-        if (_siz * 5 >= number)
-        {
-            _resultSI = true;
-        }
-        if (_pow * 5 >= number)
-        {
-            _resultP = true;
-        }
-        if (_int * 5 >= number)
-        {
-            _resultI = true;
-        }
-        if (_app * 5 >= number)
-        {
-            _resultA = true;
-        }
-        if (_con * 5 >= number)
-        {
-            _resultC = true;
-        }
-        if (_dex * 5 >= number)
-        {
-            _resultD = true;
-        }
-        if (_edu * 5 >= number)
-        {
-            _resultE = true;
-        }
-        else 
-        {
-            _resultST = false;
-            _resultC = false;
-            _resultP = false;
-            _resultD = false;
-            _resultA = false;
-            _resultSI = false;
-            _resultI = false;
-            _resultE = false;
+            _result = false;
+            _str--;
         }
     }
 
@@ -147,27 +96,28 @@ public class Dice : MonoBehaviour
                     text4.text = ("狂人が現れ、\r\n「化物!」と襲い掛かってきた！");
                     break;
             }
-            if (_resultST == false && _resultC == false && _resultA == false && _dice == true)
-            {
-                _str -= 1;
-                _app -= 1;
-            }
         }
 
     }
 
-    public void dice()
+    public void Result()
     {
-        _dice = true;
-    }
-
-    public void Andice()
-    {
-        _dice = true;
+        _result = true;
     }
 
     public void OnehunderedST()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_str * 5 >= number)
+        {
+            _resultST = true;
+        }
+        else
+        {
+            _resultST = false;
+        }
         if (_resultST) 
         {
             text.text = (_hund + "　成功");
@@ -175,11 +125,22 @@ public class Dice : MonoBehaviour
         else 
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
     }
     public void OnehunderedC()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);   // 100面ダイス用
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_con * 5 >= number)
+        {
+            _resultC = true;
+        }
+        else
+        {
+            _resultC = false;
+        }
         if (_resultC)
         {
             text.text = (_hund + "　成功");
@@ -187,12 +148,23 @@ public class Dice : MonoBehaviour
         else
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
 
     }
     public void OnehunderedP()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);   // 100面ダイス用
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_pow * 5 >= number)
+        {
+            _resultP = true;
+        }
+        else
+        {
+            _resultP = false;
+        }
         if (_resultP)
         {
             text.text = (_hund + "　成功");
@@ -200,11 +172,22 @@ public class Dice : MonoBehaviour
         else
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
     }
     public void OnehunderedD()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);   // 100面ダイス用
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_dex * 5 >= number)
+        {
+            _resultD = true;
+        }
+        else
+        {
+            _resultD = false;
+        }
         if (_resultD)
         {
             text.text = (_hund + "　成功");
@@ -212,11 +195,22 @@ public class Dice : MonoBehaviour
         else
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
     }
     public void OnehunderedA()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);   // 100面ダイス用
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_app * 5 >= number)
+        {
+            _resultA = true;
+        }
+        else
+        {
+            _resultA = false;
+        }
         if (_resultA)
         {
             text.text = (_hund + "　成功");
@@ -224,11 +218,22 @@ public class Dice : MonoBehaviour
         else
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
     }
     public void OnehunderedSI()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);   // 100面ダイス用
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_siz * 5 >= number)
+        {
+            _resultSI = true;
+        }
+        else
+        {
+            _resultSI = false;
+        }
         if (_resultSI)
         {
             text.text = (_hund + "　成功");
@@ -236,11 +241,22 @@ public class Dice : MonoBehaviour
         else
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
     }
     public void OnehunderedI()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);   // 100面ダイス用
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_int * 5 >= number)
+        {
+            _resultI = true;
+        }
+        else
+        {
+            _resultI = false;
+        }
         if (_resultI)
         {
             text.text = (_hund + "　成功");
@@ -248,11 +264,22 @@ public class Dice : MonoBehaviour
         else
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
     }
     public void OnehunderedE()
     {
+        var random = new System.Random();
+        var number = random.Next(00, 100);   // 100面ダイス用
+        _hund = number.ToString().PadLeft(2, '0');
+        if (_edu * 5 >= number)
+        {
+            _resultE = true;
+        }
+        else
+        {
+            _resultE = false;
+        }
         if (_resultE)
         {
             text.text = (_hund + "　成功");
@@ -260,95 +287,111 @@ public class Dice : MonoBehaviour
         else
         {
             text.text = (_hund + "　失敗");
-            _hp -= 1;
+            _hp--;
         }
     }
     public void Six()
     {
+        var random = new System.Random();
+        var num = random.Next(01, 07);//6面ダイス用
+        _move = num;
+        _six = num.ToString().PadLeft(1, '0');
         text.text = (_six);
         transform.position += transform.TransformDirection(Vector3.forward) * _move;//マス目移動なので今回はフレーム計算は無し。
 
     }
     public void Three()
     {
-            text.text = (_three);
+        var random = new System.Random();
+        var num2 = random.Next(01, 04);//3面ダイス用
+        _three = num2.ToString().PadLeft(1, '0');
+        text.text = (_three);
     }
     public void Four()
     {
-            text.text = (_four);
+        var random = new System.Random();
+        var num3 = random.Next(01, 05);//4面ダイス用
+        _four = num3.ToString().PadLeft(1, '0');
+        text.text = (_four);
     }
     public void ThreeToTwo()
     {
-            text.text = (_threeToTwo);
+        var random = new System.Random();
+        var num4 = random.Next(01, 04) + random.Next(01, 04);//3面2回ダイス用
+        _threeToTwo = num4.ToString().PadLeft(1, '0');
+        text.text = (_threeToTwo);
     }
     public void Twenty()
     {
-            text.text = (_twenty);
+        var random = new System.Random();
+        var num5 = random.Next(01, 21);//20面ダイス用
+        _twenty = num5.ToString().PadLeft(2, '0');
+        text.text = (_twenty);
     }
 
     public void STadd()
     {
-        _str += 1;
+        _str++;
     }
     public void Cadd()
     {
-        _con += 1;
+        _con++;
     }
     public void Padd()
     {
-        _pow += 1;
+        _pow++;
     }
     public void Dadd()
     {
-        _dex += 1;
+        _dex++;
     }
     public void Aadd()
     {
-        _app += 1;
+        _app++;
     }
     public void SIadd()
     {
-        _siz += 1;
+        _siz++;
     }
     public void Iadd()
     {
-        _int += 1;
+        _int++;
     }
     public void Eadd()
     {
-        _edu += 1;
+        _edu++;
     }
     public void STminus()
     {
-        _str -= 1;
+        _str--;
     }
     public void Cminus()
     {
-        _con -= 1;
+        _con--;
     }
     public void Pminus()
     {
-        _pow -= 1;
+        _pow--;
     }
     public void Dminus()
     {
-        _dex -= 1;
+        _dex--;
     }
     public void Aminus()
     {
-        _app -= 1;
+        _app--;
     }
     public void SIminus()
     {
-        _siz -= 1;
+        _siz--;
     }
     public void Iminus()
     {
-        _int -= 1;
+        _int--;
     }
     public void Eminus()
     {
-        _edu -= 1;
+        _edu--;
     }
 
 }
